ad7c19b2d132879ad775c01bb301d55e8b15d79a
Luiz Fernando Vital <luizfvital@gmail.com>
2026-03-01 15:48:11 -0300

    feat: add content

--- src/app/Home/styles.ts
+++ src/app/Home/styles.ts
@@ -12,5 +12,20 @@ export const styles = StyleSheet.create({
   logo: {
     height: 34,
     width: 134
+  },
+  form: {
+    width: '100%',
+    paddingHorizontal: 16,
+    gap: 16,
+    marginTop: 42,
+  },
+  content: {
+    flex: 1,
+    width: '100%',
+    backgroundColor: '#FFFFFF',
+    borderTopLeftRadius: 24,
+    borderTopRightRadius: 24,
+    padding: 24,
+    marginTop: 24,
   }
 })
